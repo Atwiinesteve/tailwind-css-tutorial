70dafd3b4e707a0599673c85b15cf9617fc22ceb
KIIZA STEPHEN <53976517+Atwiinesteve@users.noreply.github.com>
2023-03-14 14:19:34 +0000

    text customization complete

--- src/App.js
+++ src/App.js
@@ -2,8 +2,28 @@ import React from 'react';
 
 export default function App() {
   return (
-    <div className="text-3xl font-bold underline">
-      <h1>Hello World</h1>
-    </div>
-  );
+    <>
+      <div className="text-yellow-600">
+        <h1>Hello World</h1>
+      </div>
+      <div className="text-green-600">
+        <h1>Hello World</h1>
+      </div> 
+      <div className="bg-green-600 text-white">
+        <h1>Hello World</h1>
+      </div> 
+      <div className="border-4 border-blue-500">
+        <h1>Hello World</h1>
+      </div> 
+      <div className="bg-silver">
+        <h1>Hello World</h1>
+      </div> 
+      <div className="bg-tahiti-900 text-white">
+        <h1>Hello World</h1>
+      </div> 
+      <div className="bg-tahiti-200 text-red-600">
+        <h1>Hello World</h1>
+      </div> 
+    </>
+	);
 }
