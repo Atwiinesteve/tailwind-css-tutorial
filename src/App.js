8d303b83e4ed9f43f32160ce6d34fddcfe952a87
KIIZA STEPHEN <53976517+Atwiinesteve@users.noreply.github.com>
2023-03-14 15:19:12 +0000

    typography with tailwindcss

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
-import React from 'react';
+import React from "react";
 
 export default function App() {
-  return (
+	return (
 		<>
 			<div className="text-yellow-600">
 				<h1>Hello World</h1>
@@ -34,6 +34,19 @@ export default function App() {
 			</div>
 			<div>
 				<p>This is a long text</p>
+			</div>
+			<div className="leading-8 titles bg-white text-black">
+				<h1 className="text-3xl decoration-red-700 italic underline">
+					Title 1
+				</h1>
+				<h2 className="text-2xl line-through">Title 2</h2>
+				<h3 className="text-xl overline">Title 3</h3>
+				<p className="text-base capitalize">A regular paragraph</p>
+				<p className="uppercase text-sm underline decoration-blue-700 decoration-wavy underline-offset-8">
+					A description paragraph
+				</p>
+				<p className="text-xs">A little note</p>
+				<p style={{ width: '300px'}} className="truncate">This is a long sentence to explain the trancate functionality</p>
 			</div>
 		</>
 	);
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,21 +2,25 @@
 module.exports = {
 	content: ["./src/**/*.{js,jsx,ts,tsx}"],
 	theme: {
-		spacing: {
-			1: "100px",
-			2: "12px",
-			3: "16px",
-			4: "24px",
-			5: "32px",
-			6: "48px",
-		},
-		screens: {
-			sm: "640px",
-			md: "768px",
-			lg: "1024px",
-			xl: "1280px",
-		},
 		extend: {
+			fontSize: {
+				sm: '15px',
+				base: '18px',
+			},
+			spacing: {
+				1: "100px",
+				2: "12px",
+				3: "16px",
+				4: "24px",
+				5: "32px",
+				6: "48px",
+			},
+			screens: {
+				sm: "640px",
+				md: "768px",
+				lg: "1024px",
+				xl: "1280px",
+			},
 			colors: {
 				midnight: "#121063",
 				metal: "#565584",
